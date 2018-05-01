bca618fdc11d27ccb1b1747b4ab753e8db2f6733
Krico16 <bash_stone_u@hotmail.com>
2018-05-01 13:49:07 -0500

    Estilo de Página
    Añadido el estilo

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="">
+
+<!--
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin sodales lorem. Cras accumsan quam non nibh pulvinar suscipit. Nulla ullamcorper mauris eget purus volutpat, at fringilla odio faucibus. Maecenas condimentum posuere fermentum. Donec eleifend interdum orci nec rutrum. Praesent viverra tellus quis ex porta suscipit. Cras mollis odio vitae purus lacinia placerat. Fusce a tellus turpis. Suspendisse porta lacinia turpis in vehicula. Ut et suscipit nisi. Fusce maximus lectus non risus interdum, quis rutrum nunc eleifend. Maecenas eget aliquam velit, sed dictum neque.
+
+Morbi semper justo eget orci malesuada, nec ullamcorper metus gravida. Pellentesque et nibh elementum erat interdum ultrices. Nulla ligula nisi, scelerisque at velit eu, luctus venenatis nibh. Nullam non aliquam ex. Aenean laoreet faucibus ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus non arcu commodo, porta nunc ac, luctus arcu. Phasellus nunc libero, pulvinar et faucibus eget, dapibus nec lacus. Donec aliquet a risus non volutpat. Integer tempus commodo felis id viverra. Quisque tincidunt quam ut condimentum imperdiet. Mauris varius pulvinar augue, placerat fermentum leo dapibus et. Morbi at ex in risus consectetur maximus in in sapien. Quisque vulputate arcu dignissim, eleifend nibh non, pellentesque sem. Vestibulum fermentum, metus sed tempus posuere, leo ligula tempor velit, imperdiet facilisis orci arcu sed lectus.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras interdum suscipit augue quis congue. In hac habitasse platea dictumst. Sed vitae eros scelerisque, bibendum quam vitae, cursus justo. Sed non tempor sapien. Integer maximus pharetra ante ac tincidunt. Maecenas viverra egestas ex eget aliquam. Fusce pretium massa sit amet elit luctus, molestie volutpat augue faucibus. Nunc non porttitor mi. Etiam dignissim, ante semper tincidunt porttitor, turpis purus faucibus nulla, ut suscipit justo odio in urna. Cras sit amet urna nec mauris molestie cursus non non elit.
+
+Nam euismod iaculis congue. Sed convallis quam sed ipsum faucibus porttitor. Ut ut sapien sed ex finibus sagittis. Mauris fermentum efficitur venenatis. Proin finibus ipsum est, et rutrum ipsum cursus at. Duis ac mi at quam dapibus sollicitudin et nec nibh. Sed id egestas arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ac neque blandit, rhoncus felis ut, ornare diam. Aenean congue suscipit nulla non pharetra. Ut id nisl faucibus, cursus nisi vel, tincidunt nunc.
+-->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AccSoft</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</head>
+
+<body class="background">
+    <br>
+    <img src="https://www.bumbleance.com/wp-content/uploads/2013/06/banner-sample.jpg" class="banner">
+
+    <div class="bar">
+        <a href="#" class="active">Home</a>
+        <a href="#">Juegos</a>
+        <a href="#">Aplicaciones</a>
+        <a href="#">Contacto</a>
+        <a href="#">FAQ</a>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-7 blockizq">
+            <div>
+                <h1>Título 1</h1>
+                <span><h6>Por: Autor Desconocido</h6></span>
+                <img src="https://mott.pe/noticias/wp-content/uploads/2016/11/Janette-Asche.jpg">
+                <br>
+                <br>
+                <h5>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin sodales lorem. Cras accumsan quam non nibh pulvinar suscipit. Nulla ullamcorper mauris eget purus volutpat, at fringilla odio faucibus. Maecenas condimentum posuere fermentum. Donec eleifend interdum orci nec rutrum. Praesent viverra tellus quis ex porta suscipit. Cras mollis odio vitae purus lacinia placerat. Fusce a tellus turpis. Suspendisse porta lacinia turpis in vehicula. Ut et suscipit nisi. Fusce maximus lectus non risus interdum, quis rutrum nunc eleifend. Maecenas eget aliquam velit, sed dictum neque.
+                </h5>
+            </div>
+
+        </div>
+
+
+        <div class="col-3 blockder">
+
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
